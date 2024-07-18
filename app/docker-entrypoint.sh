f6087b59c3ae918d99a90c687000561124832dc7
hosseins7 <175safaei@gmail.com>
2024-07-18 12:10:25 +0330

    Update docker-entrypoint.sh

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-if [ "$DATABASE" = "mysql" ]
-then
+if [ "$DATABASE" = "mysql" ]; then
     echo "Waiting for mysql..."
     while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
+        sleep 0.1
     done
     echo "MySQL started"
 fi
 
-# Décommenter pour supprimer la bdd à chaque redémarrage (danger)
+# Uncomment to clear the database on each restart (dangerous)
 # echo "Clear entire database"
 # python manage.py flush --no-input
 
-echo "Appling database migrations..."
-COPY . /usr/src/app/
-WORKDIR /usr/src/app/
+echo "Applying database migrations..."
 
-python manage.py makemigrations 
+python manage.py makemigrations
 python manage.py migrate
 
 exec "$@"
